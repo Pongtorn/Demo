5c95e3afa346d74aa041f675e9a80cb0f34b16b0
Pongtorn <ataza11@hotmail.com>
2018-01-11 11:44:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			
+			$text = 'Hello World!!';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
