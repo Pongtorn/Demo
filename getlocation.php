87c6889b6211cf8ddcad5c99d90f7b4d13168815
Pongtorn <ataza11@hotmail.com>
2018-11-27 15:58:12 +0700

    Update getlocation.php

--- getlocation.php
+++ getlocation.php
@@ -1,144 +1,80 @@
-
 <html>
-<head>
-     <title>getlocation</title>
-</head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <head>
+    <title>Instascan &ndash; Demo</title>
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+    <script type="text/javascript" src="http://www.distributionmerchant.com/instascan.min.js"></script>
+  </head>
+  <body>
 
-<style>
-.container {
-  max-width: 980px;
-  text-align: center;
-  margin: 20px auto;
-}
-
-h1 {
-  margin-bottom: 20px;
-}
-
-#geocoding_form {
-  margin: 40px auto 40px;
-}
-
-.input-group {
-  margin-left: 4%;
-}
-
-.find-me.btn:focus {
-  border-color: transparent;
-  outline: 0;
-}
-
-.coordinates {
-  font-size: 18px;
-  opacity: 0;
-  margin-bottom: 40px;
-}
-
-.no-browser-support {
-  font-size: 18px;
-  opacity: 0;
-}
-
-.coordinates b:first-child {
-  margin-right: 15px;
-}
-
-.visible {
-  opacity: 1;
-}
-
-.map-overlay {
-  max-width: 600px;
-  height: 400px;
-  margin: 0 auto;
-  background-color: #fff;
-  position: relative;
-  border-radius: 2px;
-}
-
-#map {
-  max-width: 550px;
-  height: 400px;
-  margin: 0 auto;
-}
-
-</style>
-<script>
-function onloadpage(){
-
-var findMeButton = $('.find-me');
-
-// Check if the browser has support for the Geolocation API
-if (!navigator.geolocation) {
-
-  findMeButton.addClass("disabled");
-  $('.no-browser-support').addClass("visible");
-
-} else {
-
-  findMeButton.on('click', function(e) {
-
-    e.preventDefault();
-
-    navigator.geolocation.getCurrentPosition(function(position) {
-
-      // Get the coordinates of the current possition.
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-
-      $('.latitude').text(lat.toFixed(3));
-      $('.longitude').text(lng.toFixed(3));
-      $('.coordinates').addClass('visible');
-
-      // Create a new map and place a marker at the device location.
-      var map = new GMaps({
-        el: '#map',
-        lat: lat,
-        lng: lng
-      });
-
-      map.addMarker({
-        lat: lat,
-        lng: lng
-      });
-
-    });
-
-  });
-
-}
-
-
-}
-
-
-
-
-</script>
-
-<body onload="onloadpage()">
-
-<div class="container">
-
-  <h1>Geolocation Demo</h1>
-
-  <form id="geocoding_form" class="form-horizontal">
-    <div class="form-group">
-      <div class="col-xs-12 col-md-6 col-md-offset-3">
-        <button type="button" class="find-me btn btn-info btn-block">Find My Location</button>
+    <div id="app">
+      <div class="sidebar">
+        <section class="cameras">
+          <h2>Cameras</h2>
+          <ul>
+            <li v-if="cameras.length === 0" class="empty">No cameras found</li>
+            <li v-for="camera in cameras">
+              <span v-if="camera.id == activeCameraId" :title="formatName(camera.name)" class="active">{{ formatName(camera.name) }}</span>
+              <span v-if="camera.id != activeCameraId" :title="formatName(camera.name)">
+                <a @click.stop="selectCamera(camera)">{{ formatName(camera.name) }}</a>
+              </span>
+            </li>
+          </ul>
+        </section>
+        <section class="scans">
+          <h2>Scans</h2>
+          <ul v-if="scans.length === 0">
+            <li class="empty">No scans yet</li>
+          </ul>
+          <transition-group name="scans" tag="ul">
+            <li v-for="scan in scans" :key="scan.date" :title="scan.content">{{ scan.content }}</li>
+          </transition-group>
+        </section>
+      </div>
+      <div class="preview-container">
+        <video id="preview"></video>
       </div>
     </div>
-  </form>
-
-  <p class="no-browser-support">Sorry, the Geolocation API isn't supported in Your browser.</p>
-  <p class="coordinates">Latitude: <b class="latitude">42</b> Longitude: <b class="longitude">32</b></p>
-
-  <div class="map-overlay">
-    <div id="map"></div>
-  </div>
-
-</div>
-
-</body>
+    <script type="text/javascript">
+	  var app = new Vue({
+  el: '#app',
+  data: {
+    scanner: null,
+    activeCameraId: null,
+    cameras: [],
+    scans: []
+  },
+  mounted: function () {
+    var self = this;
+    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
+    self.scanner.addListener('scan', function (content, image) {
+      self.scans.unshift({ date: +(Date.now()), content: content });
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+      self.cameras = cameras;
+      if (cameras.length > 0) {
+        self.activeCameraId = cameras[0].id;
+        self.scanner.start(cameras[0]);
+      } else {
+        console.error('No cameras found.');
+      }
+    }).catch(function (e) {
+      console.error(e);
+    });
+  },
+  methods: {
+    formatName: function (name) {
+      return name || '(unknown)';
+    },
+    selectCamera: function (camera) {
+      this.activeCameraId = camera.id;
+      this.scanner.start(camera);
+    }
+  }
+});
+	  
+	  </script>
+  </body>
 </html>
