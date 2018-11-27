7e9421b661f9326d19420c378a0b2d3407b0a0a9
Pongtorn <ataza11@hotmail.com>
2018-11-27 15:59:39 +0700

    Update getlocation.php

--- getlocation.php
+++ getlocation.php
@@ -1,80 +1,311 @@
+
+<!DOCTYPE html>
 <html>
   <head>
-    <title>Instascan &ndash; Demo</title>
-    <link rel="icon" type="image/png" href="favicon.png">
-    <link rel="stylesheet" href="style.css">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
-    <script type="text/javascript" src="http://www.distributionmerchant.com/instascan.min.js"></script>
-  </head>
-  <body>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <link rel="canonical" href="https://www.qrstuff.com/scan"/>
+          <title>QR Code Scanner - Scan your QR Codes</title>
+              <base href="https://www.qrstuff.com/" />
+    <script>
+      var dataLayer = dataLayer || [];
+      dataLayer.push({ 'existingClient': false });</script>
+    
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5QFQNHF');</script>
+    <!-- End Google Tag Manager -->
+    
+        <link rel="stylesheet" type="text/css" href="build/styles/all-b4c0cab821.min.css" />
 
-    <div id="app">
-      <div class="sidebar">
-        <section class="cameras">
-          <h2>Cameras</h2>
-          <ul>
-            <li v-if="cameras.length === 0" class="empty">No cameras found</li>
-            <li v-for="camera in cameras">
-              <span v-if="camera.id == activeCameraId" :title="formatName(camera.name)" class="active">{{ formatName(camera.name) }}</span>
-              <span v-if="camera.id != activeCameraId" :title="formatName(camera.name)">
-                <a @click.stop="selectCamera(camera)">{{ formatName(camera.name) }}</a>
-              </span>
-            </li>
-          </ul>
-        </section>
-        <section class="scans">
-          <h2>Scans</h2>
-          <ul v-if="scans.length === 0">
-            <li class="empty">No scans yet</li>
-          </ul>
-          <transition-group name="scans" tag="ul">
-            <li v-for="scan in scans" :key="scan.date" :title="scan.content">{{ scan.content }}</li>
-          </transition-group>
-        </section>
+    <!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="css/ie8.css" /><![endif]-->
+    <!--[if lte IE 7]><link rel="stylesheet" type="text/css" href="css/ie7.css" /><![endif]-->
+    <!--[if lte IE 6]><link rel="stylesheet" type="text/css" href="css/ie6d.css" /><![endif]-->
+    <!--[if IE 5]><link rel="stylesheet" type="text/css" href="css/ie5.css" /><![endif]-->
+    <link rel="shortcut icon" href="favicon.ico" />
+    
+    
+    <script>
+      googleApiServerKey='AIzaSyDBVMxE5g-Y8R_EKdwXg7DOUjoyxB2D2eE';
+      googleMapsApiVersion='3.32';
+    </script>
+    <script>qrstuffAppData=null;</script>
+    <script>document.docReady=function(f) { if(document.readyState === 'complete') f(); else document.addEventListener('DOMContentLoaded', f); }</script>
+    <!--[if lt IE 9]>
+      <script src="build/scripts_ie8/all-019649cc24.min.js"></script>
+    <![endif]-->
+    <!--[if gte IE 9]><!-->
+      <script src="build/scripts/all-4998474a8e.min.js" defer></script>
+    <!--<![endif]-->
+    
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    
+ 
+ 
+ 
+ 
+
+<meta property="og:title" content="QR Stuff"/>
+<meta property="og:type" content="website"/>
+<meta property="og:image" content="https://www.qrstuff.com/images/opengraph.png"/>
+<meta property="og:url" content="https://www.qrstuff.com/scan"/>
+<meta property="og:site_name" content="QR Stuff"/>
+<meta property="og:description" content="QR Stuff Free QR Code Creator - Color QR codes, 20+ data types, no sign-up required"/>
+<meta property="fb:admins" content="1058772809"/>
+
+
+  <meta name="viewport" content="width=700" id="meta-viewport">
+  <script>document.docReady(function () {
+    var setViewport=function() { document.getElementById('meta-viewport').setAttribute('content', 'width='+Math.max(screen.width,700)); }
+    window.addEventListener("orientationchange",setViewport);
+    window.addEventListener("resize", _.debounce(setViewport, 200));
+    setViewport();
+  })</script>
+
+<style>.site-header, .constrain, .content-width { min-width: 700px; max-width: 950px; }</style>
+
+  
+  <script type="text/javascript" src="include/modules/custom/qrcodescan/instascan/custom/instascan.min.js"></script>
+  
+  <script src="build/scripts/scan-d83e092ab6.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
+  
+</head>
+<body class="">
+
+  <header class="site-header">
+    <!-- begin: scan-header.tpl -->
+<!-- QR Stuff - Scan - Header -->
+<div class="gad-head-responsive">
+
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7324999811165292"
+     data-ad-slot="9294044788"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+</div><!-- end: scan-header.tpl -->
+
+    <a href="https://www.qrstuff.com/" class="logo"><img src="https://img.qrstuff.com/images/top.gif" width="300" height="69"/></a>
+    <div id="client-info-box">
+      
+<form id="login-form" onsubmit="Auth.login(); return false;">
+  
+  <table class="d auto" cellpadding="0" cellspacing="0">
+    <tbody>
+      <tr>
+        <th class="login-form-label"><label for="username">Email</label></th>
+        <td><input name="username" id="login-username" class="text"  /></td>
+      </tr>
+      <tr>
+        <th class="login-form-label"><label for="password">Password</label></th>
+        <td><input name="password" id="login-password" class="text" type="password" /></td>
+      </tr>
+          </tbody>
+  </table>
+  <div id="login-buttons">
+    <span id="login-failed" style="display: none;">Login failed</span><span id="login-blocked" style="display: none;">Too many login attempts, please try again later.</span>&nbsp;
+    <a href="newaccount">Register</a>&nbsp;
+    <a href="forgotpassword">Forgot Password?</a>&nbsp;
+ 
+    <input id="login-submit" type="submit" class="submit" value="Log In" />
+  </div>
+  <div id="login-ajax" style="display: none">
+    <img src="https://img.qrstuff.com/images/g-login-ajax.gif"/>
+  </div>
+</form>
+
+
+  </div>
+    <table class="nav-tabs" cellspacing="0" cellpadding="0" border="0" height="27">
+  <tr valign=top>
+    <td width="43" align=center class=""><a href="index.html" class="top">HOME</a></td>
+    
+    <td width="3" class="none"></td>
+
+    <td width="50" align=center class=" for-anonymous"><a href="newaccount" class="top">SIGNUP</a></td>
+    <td width="3" class="none for-anonymous"></td>
+
+    <td width="50" align=center class=" for-subscribers"><a href="subscription" class="top">RENEW</a></td>
+    <td width="3" class="none for-subscribers"></td>
+
+    <td width="49" align=center class=""><a href="qr_stuff.html" class="top">ABOUT</a></td>
+    
+    <td width="3" class="none"></td>
+    
+    <td width="65" align=center class=""><a href="qr_codes.html" class="top">QR CODES</a></td>
+    
+    <td width="3" class="none"></td>
+    
+    <td width="108" align=center class=""><a href="qr_phone_software.html" class="top">PHONE SOFTWARE</a></td>
+    
+    <td width="3" class="none"></td>
+    
+    <td width="69" align=center class=""><a href="qr_code_examples.html" class="top">EXAMPLES</a></td>
+
+    <td width="3" class="none"></td>
+
+    <td width="45" align=center class=""><a href="faqs.html" class="top">FAQs</a></td>
+
+    <td width="3" class="none"></td>
+
+    <td width="75" align=center class=""><a href="affiliates.html" class="top">AFFILIATES</a></td>
+
+    <td width="3" class="none"></td>
+
+    <td width="42" align=center class="selected"><a href="scan" class="top">SCAN</a></td>
+
+  </tr>
+</table>
+
+    <div id="social-media-links">
+  <img src="https://img.qrstuff.com/images/g-social-media.png" width="115" height="24" border="0" alt="Social Media" usemap="#social-media-links-images-map" id="social-media-links-images"/>
+  <map id="social-media-links-images-map" name="social-media-links-images-map">
+    <area shape="rect" coords="0,0,24,24" href="http://www.facebook.com/qrstuff" alt="Find QR Stuff On Facebook"/>
+    <area shape="rect" coords="38,0,62,24" href="http://www.twitter.com/qrstuff" alt="Follow QR Stuff On Twitter" />
+    <area shape="rect" coords="76,0,115,24" href="https://blog.qrstuff.com/" alt="The QR Stuff Blog" />
+  </map>
+</div>
+  </header>
+
+
+  
+
+  <div class="constrain constrain-scan top">
+    <!-- begin: scan-above-scan.tpl -->
+<div class="generate-banner"><a href="https://www.qrstuff.com/"><img src="https://img.qrstuff.com/include/modules/custom/qrcodescan/images/banner_generate.jpg"/></a></div><!-- end: scan-above-scan.tpl -->
+
+    <div class="qrcodescan">
+      <h1>QR Scanner - Simply point the camera to the desired QR Code</h1>
+      <div class="preview-wrap loading">
+        <video playsinline></video>
       </div>
-      <div class="preview-container">
-        <video id="preview"></video>
+      <div class="result">
       </div>
+
+      
+      <script class="result-template" type="x-tmpl-mustache">
+        
+          <div>
+            <h2>Scan Result</h2>
+            <div class="result-content{{#scan.displayMultiline}} multiline{{/scan.displayMultiline}}">
+              {{#scan.isLink}}{{scan.linkIntro}} <a href="{{scan.link}}" target="_blank">{{&scan.html}}</a>{{/scan.isLink}}
+              {{^scan.isLink}}{{&scan.html}}{{/scan.isLink}}
+            </div>
+            <form class="ng"><button class="scan-button button">Scan Another QR Code</button></form>
+          </div>
+        
+      </script>
+      
+
     </div>
-    <script type="text/javascript">
-	  var app = new Vue({
-  el: '#app',
-  data: {
-    scanner: null,
-    activeCameraId: null,
-    cameras: [],
-    scans: []
-  },
-  mounted: function () {
-    var self = this;
-    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
-    self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({ date: +(Date.now()), content: content });
-    });
-    Instascan.Camera.getCameras().then(function (cameras) {
-      self.cameras = cameras;
-      if (cameras.length > 0) {
-        self.activeCameraId = cameras[0].id;
-        self.scanner.start(cameras[0]);
-      } else {
-        console.error('No cameras found.');
-      }
-    }).catch(function (e) {
-      console.error(e);
-    });
-  },
-  methods: {
-    formatName: function (name) {
-      return name || '(unknown)';
-    },
-    selectCamera: function (camera) {
-      this.activeCameraId = camera.id;
-      this.scanner.start(camera);
-    }
-  }
-});
-	  
-	  </script>
+
+    <div class="scan-about">
+      <h2>QR Code Scanner</h2>
+       
+      <p>The QR Stuff QR Code Scanner is the perfect tool to scan your QR codes anytime and anywhere. Now you can easily and simply  Generate, Download and Scan your QR codes all in one place – QRStuff.com.</p>
+
+      <h3>How To Scan QR Codes With QR Stuff</h3>
+
+      <p>Once you see a QR code -- whether as payment gateways, tourist information, videos, or any other QR code type - use your device camera to capture the QR code. Simply point the camera at the QR code to be scanned and you’re done. The first time you scan a QR code with us you be asked to give our QR code scanner permission to access the camera on your device, but after that, it’s straight down to business every time.</p>
+
+      <h3>QR Code Scanning For Both Desktop And Mobile</h3>
+       
+      <h3>Welcome to browser-based QR code scanning</h3>
+
+      <p>For Android and iOS mobile devices simply visit this page (<a href="https://www.qrstuff.com/scan">https://www.qrstuff.com/scan</a>) in your browser and create a home screen shortcut to quickly access our QR Code Reader directly from your smartphone's home screen.</p>
+
+      <p>Our QR Code Reader also works in both Chrome and Safari on your laptop by using the built-in camera to scan the QR code so you can create a desktop icon on your laptop as well for one-touch scanning.</p>
+    </div>
+
+  </div>
+
+
+  <footer class="site-footer content-width" style="margin: auto;">
+    <!-- begin: scan-footer.tpl -->
+<!-- QR Stuff - Scan - Footer -->
+<div class="gad-foot-responsive gad-scan">
+
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7324999811165292"
+     data-ad-slot="4560592853"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+</div><!-- end: scan-footer.tpl -->
+
+    <table cellspacing="0" cellpadding="0" border="0" align="center" style="width: 100%;">
+      <tr valign=top>
+        <td class="none">
+          <a href="https://www.qrstuff.com/index.html"><img src="https://img.qrstuff.com/images/bottomlogo.gif" width="63" height="31" vspace="0" hspace="0" border="0" ALT="QR Code Generator"/></a><BR>
+        </td>
+        <td align="right" class="none footer-nav">
+          <font class="bottom">
+            <a href="index.html" class="bottom" TITLE="QR Code Generator">Home</a>&nbsp;|
+            <a href="https://www.qrstuff.com/" class="bottom">QR Code Generator</a>&nbsp;|
+            <a href="qr_stuff.html" class="bottom">About This Site</a>&nbsp;|
+            <a href="qr_codes.html" class="bottom">What's A QR Code?</a>&nbsp;|
+            <a href="qr_phone_software.html" class="bottom">Phone Software</a>&nbsp;|
+            <a href="qr_code_examples.html" class="bottom">QR Code Examples</a>&nbsp;|
+            <a href="faqs.html" class="bottom">FAQs</a>&nbsp;|
+            <a href="scan" class="bottom">Scan a QR Code</a>&nbsp;|
+            <a href="terms.pdf" class="bottom" target="_blank">Terms Of Use</a>&nbsp;|
+            <a href="contact.html" class="bottom">Contact Us</a>
+          </font>
+        </td>
+      </tr>
+      <tr>
+        <td class="none footer-copy" colspan="2">
+          <font class="bottom">
+            Website &copy;2017 <a href="https://www.qrstuff.com/" class="bottom" TITLE="QR Code Generator">QRStuff</a> | QRStuff is a registered trademark | QR Code is a registered trademark of DENSO WAVE INCORPORATED
+          </font>
+        </td>
+      </tr>
+    </table>
+    <img src="https://img.qrstuff.com/images/space.gif" width="1" height="12" vspace="0" hspace="0" border="0"/><BR>
+  </footer>
+
+
+    <div id="fb-root"></div>
+    <script type="text/javascript" src="https://connect.facebook.net/en_US/all.js#xfbml=1&amp;appId=444307425589478"></script>
+    
+    
+<script id="dialog-template" type="x-tmpl-mustache">
+<div class="dialog-contain"{{#dialogId}} id="{{dialogId}}"{{/dialogId}}>
+  <div class="dialog-box-mask dialog-close"></div>
+  <div class="dialog-wrapper">
+    <div class="dialog-box">
+      {{#topClose}}<div class="close dialog-close"><img src="https://img.qrstuff.com/images/icons/g-close.png" width="16" height="16" alt="Close"/></div>{{/topClose}}
+      
+      {{#url}}<iframe src="{{url}}" style="{{#width}}width: {{width}}px; {{/width}}{{#height}}height: {{height}}px; {{/height}}"></iframe>{{/url}}
+      {{#message}}<div class="inner-text"><p>{{message}}</p></div>{{/message}}
+      {{#messageHtml}}<div class="inner-text">{{&messageHtml}}</div>{{/messageHtml}}
+      {{#offerSubscribe}}
+      <div class="actions">
+        <a href="index.html" class="btn secondary no-btn dialog-close">No Thanks</a>
+        <a href="newaccount#signup" class="btn primary yes-btn">Yep, Sign Me Up</a>
+        <a href="newaccount" class="btn more-info-btn">More Info</a>
+      </div>
+      {{/offerSubscribe}}
+      {{#footerClose}}<div class="button dialog-close">Close</div>{{/footerClose}}
+      {{#closeButtonText}}
+        <div class="actions centered">
+          <a href="#" class="btn primary no-btn dialog-close">{{closeButtonText}}</a>
+        </div>
+        {{/closeButtonText}}
+      </div>
+      
+    </div>
+  </div>
+</div>
+</script>
   </body>
-</html>
+</html>      
